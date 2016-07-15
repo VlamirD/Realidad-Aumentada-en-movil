31d1e3fc9076c0c7c911f53b4056c24b62800106
vladimir chocce aparco <vladimir chocce aparco>
2016-07-15 11:44:10 -0500

    diseñando la interfas del aplicativo

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,19 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  #Idpeligro */
+    $(document).on("click", "#Idpeligro", function(evt)
+    {
+        /* your code goes here */ 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
